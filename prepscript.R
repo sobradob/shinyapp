617475c4255189ed8ab7fff6632501b15f5e1044
Boaz Sobrado <sobradob@gmail.com>
2017-06-05 11:15:16 +0200

    first working prototype

--- prepscript.R
+++ prepscript.R
@@ -4,7 +4,7 @@ library(readr)
 
 d<-readRDS("C:\\Users\\user1\\Desktop\\Life\\Professional\\Statistics Nederland\\gps\\peterLoc.rds")
 
-data10k<-data %>% top_n(10000,time) %>% select(lat,lon, velocity, accuracy)
+data10k<-d %>% top_n(10000,time) %>% select(lat,lon, velocity, accuracy,time)
 data<-data10k
 write_csv(x = data10k,path="test.csv")
 
